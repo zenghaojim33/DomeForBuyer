6e0eda41ef3016603418feb8e3fc973f283124fc
Jenk <jenkshow@163.com>
2015-04-14 11:00:03 +0800

    Initial Commit

---
+++ DomeForSell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DomeForSell
+//
+//  Created by BTW on 15/4/14.
+//  Copyright (c) 2015年 jenk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DomeForSell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DomeForSell
+//
+//  Created by BTW on 15/4/14.
+//  Copyright (c) 2015年 jenk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
